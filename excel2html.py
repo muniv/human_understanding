80f970c384cf4b0048e49a4f839fdd093136020d
wonchul_kim <wonchul_kim@lotte.net>
2023-03-29 06:58:44 +0000

    init

---
+++ excel2html.py
@@ -0,0 +1,6 @@
+from xlsx2html import xlsx2html
+
+
+xlsx2html('[DRM해제]위플젝 메뉴얼_텍스트_v 0.02_20230327.xlsx', 'output_1.html', sheet='위플젝 메뉴얼 구성')
+xlsx2html('[DRM해제]위플젝 메뉴얼_텍스트_v 0.02_20230327.xlsx', 'output_2.html', sheet='1. 개요')
+xlsx2html('[DRM해제]위플젝 메뉴얼_텍스트_v 0.02_20230327.xlsx', 'output_3.html', sheet='2. 공통기능')
